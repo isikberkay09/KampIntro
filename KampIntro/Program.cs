c5aa58b01ef08b91db512e371e9596da578aad3a
isikberkay <isikberkay@DESKTOP-D1417BO>
2021-10-11 01:19:23 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety - tip güvenliği
+            //do not repeat yourself - kendini tekrar etme
+            //kategori etiketi = değer tutucu,alias
+
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = true;
+            double dolarDun = 7.45;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalış butonunu göster");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış butonunu göster");
+            }
+            else
+            {
+                Console.WriteLine("Eşittir butonunu göster");
+            }
+
+            if (sistemeGirisYapmisMi==true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giriş yap butonu");
+            }
+
+
+            Console.WriteLine(kategoriEtiketi);
+
+        }
+    }
+}
